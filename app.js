0f5622a66cb9956f1464b388cfc6c24548edf695
vHades <anhvu.nd.0199@gmail.com>
2021-09-22 10:18:10 +0700

    update

--- app.js
+++ app.js
@@ -4,26 +4,6 @@ const { readFileSync, writeFileSync, appendFileSync } = require('fs');
 const pathSrc = process.argv[2].replace(__dirname, '.').toUpperCase();
 const fSrc = path.parse(pathSrc);
 const src = readFileSync(pathSrc, fSrc.ext == '.DAT' ? 'utf16le' : 'utf8');
-let transFile;
-
-if (process.argv[3] && process.argv[3].toLowerCase() == 'sort') {
-    const sortGroupFunc = (a, b) => {
-        if (a == 'original') return -1;
-        if (b == 'original') return 1;
-        if (a == 'Common') return 1;
-        if (b == 'Common') return -1;
-
-        return a.localeCompare(b);
-    };
-    const doc = yaml.load(src);
-    Object.keys(doc).forEach(k => {
-        doc[k] = doc[k].sort((a, b) => {
-            return a.original.localeCompare(b.original);
-        })
-    });
-    const text = yaml.dump(doc, { sortKeys: sortGroupFunc, quotingType: '"', lineWidth: -1 });
-    writeFileSync(pathSrc, text, { encoding: 'utf8' });
-}
 
 let dest = '';
 const convertToDAT = (node) => {
@@ -71,23 +51,94 @@ const convertToYaml = (lines) => {
     return node;
 }
 
-switch (fSrc.ext) {
-    case '.YAML':
-        const doc = yaml.load(src);
-        convertToDAT(doc);
-        transFile = path.join(fSrc.dir, `${fSrc.name}.DAT`);
-        writeFileSync(transFile, `[TRANSLATIONS]\n${dest}[/TRANSLATIONS]\n`, { encoding: 'utf16le' });
-        break;
+const getObjFromSrc = () => {
+    switch (fSrc.ext) {
+        case '.YAML':
+            break;
 
-    case '.DAT':
-        const obj = convertToYaml(src.split('\n').filter(x => x && !x.toUpperCase().endsWith('TRANSLATIONS]')))
-        transFile = path.join(fSrc.dir, `${fSrc.name}.YAML`);
-        writeFileSync(transFile, yaml.dump(obj, { sortKeys: true, quotingType: "'" }), { encoding: 'utf8' });
-        break;
+        case '.DAT':
+            const lines = src
+                .split('\n')
+                .filter(x => x && !x.toUpperCase().endsWith('TRANSLATIONS]'))
+            return convertToYaml(lines);
 
-    default:
-        break;
+        default:
+            console.error('File is not support!!!')
+            break;
+    }
 }
 
+const writeYamlFile = (filePath, doc) => {
+    const strDump = typeof doc === 'string' ? doc : yaml.dump(doc, {
+        sortKeys: true, quotingType: '"', lineWidth: -1
+    }).replace(/^(?!\s)/gm, '\n').trimStart();
+    writeFileSync(filePath, strDump, { encoding: 'utf8' });
+}
 
+switch (process.argv[3]) {
+    // {file}.[YAML] sort
+    case 'sort':
+        if (fSrc.ext != '.YAML') {
+            console.error('Is not a YAML file!');
+            break;
+        }
+        const sortGroupFunc = (a, b) => {
+            if (a == 'original') return -1;
+            if (b == 'original') return 1;
+            if (a == 'Common') return 1;
+            if (b == 'Common') return -1;
 
+            return a.localeCompare(b);
+        };
+        const doc = yaml.load(src);
+        Object.keys(doc).forEach(k => {
+            doc[k] = doc[k].sort((a, b) => {
+                return a.original.localeCompare(b.original);
+            });
+            console.info(` [${doc[k].length}] ${k}`);
+        });
+        const text = yaml.dump(doc, {
+            sortKeys: sortGroupFunc, quotingType: '"', lineWidth: -1
+        }).replace(/^(?!\s)/gm, '\n').trimStart();
+        writeYamlFile(pathSrc, text);
+        break;
+
+    // {file}.[YAML|DAT] merge
+    case 'merge':
+        const mergeObj = getObjFromSrc();
+        const rootPath = './media/TRANSLATIONS/VIETNAMESE/TRANSLATION.YAML';
+        const root = yaml.load(readFileSync(rootPath, 'utf8'));
+        Object.keys(root).forEach(k => {
+            root[k].forEach((item, inx) => {
+                const newItem = mergeObj.Common.find(x => x.original == item.original);
+                if (newItem?.translation)
+                    root[k][inx].translation = newItem.translation;
+            })
+        });
+        writeYamlFile(rootPath, root);
+        break;
+
+    // {file}.[YAML|DAT]
+    default:
+        let transFile;
+        switch (fSrc.ext) {
+            case '.YAML':
+                const doc = yaml.load(src);
+                convertToDAT(doc);
+                transFile = path.join(fSrc.dir, `${fSrc.name}.DAT`);
+                writeFileSync(transFile, `[TRANSLATIONS]\n${dest}[/TRANSLATIONS]\n`, { encoding: 'utf16le' });
+                break;
+
+            case '.DAT':
+                const obj = getObjFromSrc();
+                transFile = path.join(fSrc.dir, `${fSrc.name}.YAML`);
+                writeFileSync(transFile, yaml.dump(obj, {
+                    sortKeys: true, quotingType: '"', lineWidth: -1
+                }), { encoding: 'utf8' });
+                break;
+
+            default:
+                break;
+        }
+        break;
+}
